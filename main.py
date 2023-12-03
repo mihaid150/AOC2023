824acd125f107c9a9da4553c4d5bc5bdbe7f56f5
mihai150 <mdaian150@yahoo.com>
2023-12-03 14:09:22 +0200

    First AOC2023 repo init

---
+++ main.py
@@ -0,0 +1,9 @@
+from day1.calibration import extract_calibration_sum
+from day1.calibration2 import extract_calibration_sum2
+
+# Day1 part 1
+# print(extract_calibration_sum())
+
+# Day1 part 2
+print(extract_calibration_sum2())
+
---
+++ read_input.py
@@ -0,0 +1,7 @@
+
+def get_input(file_path):
+
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    return content
