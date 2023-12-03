b451f8857035cd2083b12010adf3bce053ee41c9
mihai150 <mdaian150@yahoo.com>
2023-12-03 16:54:09 +0200

    updated main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from day1.calibration import extract_calibration_sum
 from day1.calibration2 import extract_calibration_sum2
 from day2.possible_games import get_sum_of_possible_games
+from day2.power_set_of_cubes import get_power_set_of_cubes
 
 # Day1 part 1
-# print(extract_calibration_sum())
+# extract_calibration_sum()
 
 # Day1 part 2
-# print(extract_calibration_sum2())
+# extract_calibration_sum2()
 
 # Day2 part 1
-get_sum_of_possible_games()
+# get_sum_of_possible_games()
 
+# Day 2 part 2
+get_power_set_of_cubes()
