ad950cb50057b0c5783d4a14b55a79876747dae2
mihai150 <mdaian150@yahoo.com>
2023-12-06 15:07:48 +0200

    upated main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from day1.calibration import extract_calibration_sum
 from day1.calibration2 import extract_calibration_sum2
 from day2.possible_games import get_sum_of_possible_games
 from day2.power_set_of_cubes import get_power_set_of_cubes
+from day3.sum_adjacent_parts import EngineSchematicData
+from read_input import get_input
 
 # Day1 part 1
 # extract_calibration_sum()
@@ -13,4 +15,10 @@ from day2.power_set_of_cubes import get_power_set_of_cubes
 # get_sum_of_possible_games()
 
 # Day 2 part 2
-get_power_set_of_cubes()
+# get_power_set_of_cubes()
+
+# Day 3 part 1
+engine = EngineSchematicData(get_input("day3/input.txt"))
+print(engine.compute_engine_sum())
+
+
