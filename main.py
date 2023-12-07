fe619ebd99cb9b22c009572b8fd3d12c643550ac
mihai150 <mdaian150@yahoo.com>
2023-12-07 16:27:01 +0200

    updated main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from day1.calibration2 import extract_calibration_sum2
 from day2.possible_games import get_sum_of_possible_games
 from day2.power_set_of_cubes import get_power_set_of_cubes
 from day3.sum_adjacent_parts import EngineSchematicData
+
 from read_input import get_input
 
 # Day1 part 1
@@ -22,7 +23,10 @@ from read_input import get_input
 # print(engine.compute_engine_sum())
 
 # Day 3 part 2
-engine = EngineSchematicData(get_input("day3/input.txt"))
-print(engine.compute_sum_of_all_gear_ratios())
+# engine = EngineSchematicData(get_input("day3/input.txt"))
+# print(engine.compute_sum_of_all_gear_ratios())
+
+# Day 4 part 1
+
 
 
